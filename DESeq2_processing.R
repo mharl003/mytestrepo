1923d90fca66a8c60724f58ebfe8c9c95e48a1a6
andresn <andres.narva@gmail.com>
2021-03-02 12:33:42 -0800

    Fully functional BWA and DESeq2 creating a volcano plot of differential expression via Marcus

---
+++ DESeq2_processing.R
@@ -0,0 +1,34 @@
+#!/usr/bin/env Rscript
+
+library ('DESeq2')
+
+#code to run
+directory<-'/bigdata/jinkersonlab/shared/space/sandbox/RNASeq_template_Heinz/results/mutant_vs_wildtype_leaf'
+sampleFiles<-grep('count', list.files(directory), value=TRUE)
+sampleFiles
+sampleCondition<-c('M', 'M', 'M', 'M', 'W', 'W', 'W', 'W', 'W')
+sampleTable<-data.frame(sampleName=sampleFiles, fileName=sampleFiles, condition=sampleCondition)
+
+sampleTable
+
+ddsHTSeq<-DESeqDataSetFromHTSeqCount(sampleTable=sampleTable, directory=directory, design=~condition)
+
+ddsHTSeq
+
+colData(ddsHTSeq)$condition<-factor(colData(ddsHTSeq)$condition, levels=c('M','W'))
+
+dds<-DESeq(ddsHTSeq)
+
+res<-results(dds)
+res<-res[order(res$padj),]
+head(res)
+
+plotMA(dds,ylim=c(-3,3),main='DESeq2')
+dev.copy(png,'M_vs_W.png')
+dev.off()
+
+mcols(res,use.names=TRUE)
+
+write.csv(as.data.frame(res),file='SPACE_tomato_Mutant_vs_WT.csv')
+
+summary(res)
